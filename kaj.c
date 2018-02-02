cfa79ca5542700f47763a6fc6eb8f9ee4726a316
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-02 16:49:29 +0530

    Create kaj.c

---
+++ kaj.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr;
+ int i, j;
+    for(i=1;i<=5;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+       printf("%c",'A' + j-1);
+      }
+      printf("\n");
+    }
+getch();
+}
